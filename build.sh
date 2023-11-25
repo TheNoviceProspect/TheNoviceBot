e53e1f17cec18c63049e767eeef3f700957af9f5
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2023-11-25 21:59:02 +0000

    build.sh fix to copy token

--- build.sh
+++ build.sh
@@ -15,3 +15,6 @@ common="--nologo --verbosity $verbosity"
 dotnet clean $common -c $configuration
 dotnet restore $common --force-evaluate --use-lock-file -f
 dotnet build $common --no-restore --self-contained -c $configuration
+
+## A "hacky" way to make sure we have our token present next to the app
+cp ./TWITCH.SECRET ./app/src/bin/Release/net8.0/linux-x64/
